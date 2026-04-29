656f2594ad295cec66178f41b6d2d06741d67f5b
purplekraken <matvei.sigidin.07@gmail.com>
2026-04-30 01:28:12 +0500

    v0.1
    Черновой вариант создание проекта

---
+++ Tempora/Tempora/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Tempora
+//
+//  Created by Матвей  on 30.04.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
